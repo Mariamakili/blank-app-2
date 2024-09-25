154dfcb00815a7798474a86eae381d8f3929f24e
Mariamakili <mariamakili4@gmail.com>
2024-09-25 18:52:46 +0000

    New code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,40 @@
 import streamlit as st
 
 st.title("🎈 exercise.py")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+# Load the dataset
+@st.cache_data()
+def load_data():
+    df = pd.read_csv('unicorns.csv')
+    return df
+
+# Pre-process the data
+def pre_process(df):
+    df['Date Joined'] = pd.to_datetime(df['Date Joined'])
+    return df
+
+st.title('Unicorns')
+
+df = load_data()
+df = pre_process(df)
+
+st.header('Companies by Country')
+
+fig, ax = plt.subplots(figsize=(6.4, 2.4))
+
+# Count the number of companies by country
+# TODO
+
+# Plot the number of companies by country
+# TODO
+
+ax.set_title('Number of Companies by Country')
+ax.set_xlabel('Country')
+ax.set_ylabel('Number of Companies')
+
+st.pyplot(fig)
+
